6e8d88b0c942b0338f6c41eaef63f1102cb69ff9
Abdellah Moungid <60260252+Abdell-11@users.noreply.github.com>
2023-04-21 04:20:06 +0000

    aifhnaopf

--- src/pages/graph.tsx
+++ src/pages/graph.tsx
@@ -1,5 +1,10 @@
 import { useRouter } from "next/router";
+// import  {type GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { api } from "~/utils/api";
+import type { sensorData } from "@prisma/client";
+// import { createClient } from '@supabase/supabase-js'
+import axios from 'axios'
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,7 +16,38 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { faker } from "@faker-js/faker";
+
+
+// export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+//   const dataName = context.query.dataName as string
+//   const supabase = createClient('https://arbotauyvnobapaukzfi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyYm90YXV5dm5vYmFwYXVremZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA3Mzg2NjksImV4cCI6MTk5NjMxNDY2OX0.gGP7TkmkwurTbsWfOxt9umZYiKXb2Jp4cmKjnnK-dUg')
+//   const { data, error } = await supabase
+//   .from('sensordata')
+//   .select('*')
+//   .eq('node_id', 1)
+//   .order('created_at', { descending: true })
+
+//   if (error) {
+//     console.log(error)
+//     return {
+//       notFound: true,
+//     }
+//   }
+//   return {
+//     props: { dataName, data }, // will be passed to the page component as props
+//   }
+// }
+
+axios.get('../api/sensorData/graphData')
+  .then(response => {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(error => {
+    // handle error
+    console.log(error);
+  });
+
 
 ChartJS.register(
   CategoryScale,
@@ -23,6 +59,7 @@ ChartJS.register(
   Legend
 );
 
+
 export const options = {
   responsive: true,
   plugins: {
@@ -31,39 +68,65 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: '',
     },
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-const SensorDataPage = () => {
-  const router = useRouter();
-  const dataName = router.query.dataName as string;
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const SensorDataPage = ({
+  dataName,
+  data,
+}: 
+// InferGetServerSidePropsType<typeof getServerSideProps>) => {
+
+  //const { data: sensors, isLoading, error } = api.sensorData.graphData.useQuery();
+  
+  // const dataHelper = (dataName?: "gas" | "temperature" | "soil" | "pressure" | "humidity") => {
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+  
+    if (error || !sensors) {
+      return null;
+    }
+    switch (dataName) {
+      case "gas":
+        return sensors.gas;
+      case "temperature":
+        return sensors.temperature;
+      case "soil":
+        return sensors.soil;
+      case "pressure":
+        return sensors.pressure;
+      case "humidity":
+        return sensors.humidity;
+      default:
+        return null;
+    }
+  //  }
+
+   const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => Math.random() * 100) ,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  }
 
   return (
-    <div>
+    <strong><div>
       <h1>{dataName} Data</h1>
       <Line options={options} data={data} />
-    </div>
+    </div></strong>
   );
 };
 
